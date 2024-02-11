528ed66fe88550729984eb64bbc0d1cb92d7330f
fahed <zidanifahd@gmail.com>
2024-02-11 16:17:00 +0100

    project setup

---
+++ resumeapi/src/main/java/com/faheDevs/resumeapi/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.faheDevs.resumeapi.token;
+
+public enum TokenType {
+  BEARER
+}
---
+++ resumeapi/src/test/java/com/faheDevs/resumeapi/ResumeapiApplicationTests.java
@@ -0,0 +1,8 @@
+package com.faheDevs.resumeapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeapiApplicationTests {
+
+}
